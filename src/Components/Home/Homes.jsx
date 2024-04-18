5e0c2e4f75b27632baa8bd4d71cb08cbabb0e192
emon00007 <skemon152@gmail.com>
2024-04-18 04:06:44 -0700

    last update done

--- src/Components/Home/Homes.jsx
+++ src/Components/Home/Homes.jsx
@@ -7,10 +7,10 @@ import { Helmet } from "react-helmet";
 const Homes = () => {
     const homes = useLoaderData()
     return (
-        <div className=" ">
+        <div className=" text-center">
             <Helmet><title>Home</title></Helmet>
             <BannerSlider></BannerSlider>
-
+                <h2 className="lg:my-10 my-5 font-semibold md:font-bold lg:font-bold md:text-3xl lg:text-4xl text-2xl"> Choose your home </h2>
             <div className="grid mx-2 md:mx-4 lg:mx-5  md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-5 my-6 ">
                 {homes.map(home => <Home key={home.id} homes={home}></Home>)}
             </div>
